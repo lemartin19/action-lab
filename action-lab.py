52bdba8b271715c62e7f1031738fb33264627474
lemartin19 <martin.ly@husky.neu.edu>
2018-03-14 18:02:22 -0400

    Update access and database
    Created a GameData database, allow anyone to create a user account

--- action-lab.py
+++ action-lab.py
@@ -9,6 +9,7 @@ import re
 import time
 import webapp2
 from google.appengine.ext import db
+import xml.etree.ElementTree as ET
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -24,22 +25,26 @@ values  = {"first_name": "",
     "sex_error": "",
     "message": ""}
 SECRET = "supersupersalty"
-ADMIN_PASS = "action_lab2017"
+ADMIN_PASS = "action_lab2018"
 
 class Feedback(db.Model):
   name = db.StringProperty()
   created = db.DateTimeProperty()
   feedback = db.TextProperty()
-
+  
+class GameData(db.Model):
+  game = db.IntegerProperty()
+  user = db.IntegerProperty()
+  trials = db.IntegerProperty()
+  points = db.IntegerProperty()
+  time = db.DateTimeProperty()
+  data = db.TextProperty()
+  
 class User(db.Model):
   first_name = db.StringProperty()
   last_name = db.StringProperty()
-  dob = db.StringProperty()
+  dob = db.DateTimeProperty()
   sex = db.StringProperty()
-  game1 = db.IntegerProperty()
-  game2 = db.IntegerProperty()
-  game3 = db.IntegerProperty()
-  game4 = db.IntegerProperty()
 
 #Checks if user is an admin
 def check_admin(admin):
@@ -48,7 +53,8 @@ def check_admin(admin):
 #Checks if user is signed in to play games
 def check_user(this_user):
   try: 
-    return User.get_by_id(int(this_user.split('|')[0])) and (return_hash(this_user.split('|')[0]) == this_user.split('|')[1])
+    user_code = this_user.split('|')
+    return User.get_by_id(int(user_code[0])) and (return_hash(user_code[0]) == user_code[1])
   except:
     return False
 
@@ -81,9 +87,9 @@ def valid_user(first_name,last_name,dob,sex):
   try:
     if not this_user:
       values['first_name_error'] = "Name not in database. Input name with capital first letter (e.g. Xxxxxx)"
-    if this_user.dob != return_hash(dob):
+    if this_user.dob != dob:
       values['dob_error'] = "Birthday doesn't match database. Input birthday with month, day, and year split by '/' (e.g. MM/DD/YYYY)"
-    if this_user.sex != return_hash(sex):
+    if this_user.sex != sex:
       values['sex_error'] = "Sex doesn't match database. Input first character of sex (e.g. 'M'/'F')"
     else :
       update_values()
@@ -127,24 +133,15 @@ class AddUser(webapp2.RequestHandler):
   def get(self):
     logging.info("**** AddUser Get ****")
     update_values()
-    if not check_admin(self.request.cookies.get('admin')):
-      self.response.headers.add_header('Set-Cookie', 'error="You are not authorized to view this page. Please login as an admin."; Path=/')
-      self.response.headers.add_header('Set-Cookie', 'admin=""; Expires=%s' % (datetime.datetime.now() + datetime.timedelta(-1)))
-      self.redirect('/error')
     self.write_form(**values)
   
   def post(self):
     logging.info("**** AddUser Post ****")
-    
-    if not check_admin(self.request.cookies.get('admin')):
-      self.response.headers.add_header('Set-Cookie', 'error="You are not authorized to view this page. Please login as an admin."; Path=/')
-      self.response.headers.add_header('Set-Cookie', 'admin=""; Expires=%s' % (datetime.datetime.now() + datetime.timedelta(-1)))
-      self.redirect('/error')
-    
     global values
     values['first_name'] = escape_html(self.request.get("first_name"))
     values['last_name'] = escape_html(self.request.get("last_name"))
     values['dob'] = escape_html(self.request.get("dob"))
+    
     values['sex'] = escape_html(self.request.get("sex"))
     values['message'] = "User already in database"
     
@@ -152,12 +149,9 @@ class AddUser(webapp2.RequestHandler):
       user = User()
       user.first_name = values['first_name']
       user.last_name = values['last_name']
-      user.dob = return_hash(values['dob'])
-      user.sex = return_hash(values['sex'])
-      user.game1 = 0
-      user.game2 = 0
-      user.game3 = 0
-      user.game4 = 0
+      dob = map(int, values['dob'].split('/'))
+      user.dob = datetime.datetime(dob[2], dob[0], dob[1])
+      user.sex = values['sex']
       user.put()
       update_values(message="User added")
       logging.info("here")
@@ -185,7 +179,7 @@ class AdminLogin(webapp2.RequestHandler):
     
   def post(self):
     logging.info("**** AdminLogin Post ****")
-    if check_admin(return_hash(escape_html(self.request.get("admin")))):
+    if check_admin(return_hash(escape_html(self.request.get('admin')))):
       self.response.headers.add_header('Set-Cookie', 'admin=%s; Path=/' % return_hash(ADMIN_PASS))
       self.redirect('/home')
     self.write_form(error="Incorrect Password",message="")
@@ -198,23 +192,35 @@ class AdminLogin(webapp2.RequestHandler):
 class Data(webapp2.RequestHandler):
   def get(self):
     logging.info("**** Data Get ****")
-    if not (check_admin(self.request.cookies.get("admin"))):
-      self.response.headers.add_header('Set-Cookie','error="You must login as an admin in order to continue to this site."; Path=/')
-      self.response.headers.add_header('Set-Cookie', 'admin=""; Path=/')
-      self.response.headers.add_header('Set-Cookie', 'user=""; Path=/')
+    if not check_admin(self.request.cookies.get('admin')):
+      self.response.headers.add_header('Set-Cookie', 'error="You are not authorized to view this page. Please login as an admin."; Path=/')
+      self.response.headers.add_header('Set-Cookie', 'admin=""; Expires=%s' % (datetime.datetime.now() + datetime.timedelta(-1)))
       self.redirect('/error')
+    game_data = GameData.all()
+    game_data.order('-user')
     template = JINJA_ENVIRONMENT.get_template('templates/data.html')
-    self.response.write(template.render(title="Data", user=self.request.cookies.get("user"), admin=self.request.cookies.get("admin")))
-  
+    self.response.write(template.render(title="Data", user=self.request.cookies.get("user"), admin=self.request.cookies.get("admin"), game_data=game_data))
+
   def post(self):
-    logging.info("**** Data Post ****")
-    if not (check_admin(self.request.cookies.get("admin"))):
-      self.response.headers.add_header('Set-Cookie','error="You must login as an admin in order to continue to this site."; Path=/')
+    logging.info("**** Data post ****")
+    if not (check_admin(self.request.cookies.get("admin")) or check_user(self.request.cookies.get("user"))):
+      self.response.headers.add_header('Set-Cookie','error="You must login as either an admin or user in order to continue to this site."; Path=/')
       self.response.headers.add_header('Set-Cookie', 'admin=""; Path=/')
       self.response.headers.add_header('Set-Cookie', 'user=""; Path=/')
-      self.redirect('/error')
-    template = JINJA_ENVIRONMENT.get_template('templates/data.html')
-    self.response.write(template.render(title="Data", user=self.request.cookies.get("user"), admin=self.request.cookies.get("admin")))
+    game_data = self.request.body
+    gd = ET.fromstring(game_data)
+    
+    gd_entry = GameData()
+    gd_entry.game = int(gd.find('game').text)
+    user = int(self.request.cookies.get("user").split('|')[0] if self.request.cookies.get("user") else -1)
+    logging.info(user)
+    gd_entry.user = user
+    gd_entry.trials = int(gd.find('trials').text)
+    gd_entry.points = int(gd.find('points').text)
+    gd_entry.time = datetime.datetime.now()
+    gd_entry.data = gd.find('data').text
+    gd_entry.put()
+    time.sleep(0.2)
     
 #Default home page to redirect to other pages
 class Default(webapp2.RequestHandler):
@@ -228,7 +234,6 @@ class Default(webapp2.RequestHandler):
 #Something isnt right
 class Error(webapp2.RequestHandler):
   def get(self):
-    #global values
     logging.info("**** Error Get ****")
     template = JINJA_ENVIRONMENT.get_template('templates/error.html')
     self.response.write(template.render(title="Error", user=self.request.cookies.get("user"), admin=self.request.cookies.get("admin"), message=self.request.cookies.get('error')))
@@ -242,7 +247,7 @@ class Files(webapp2.RequestHandler):
     template = JINJA_ENVIRONMENT.get_template('templates/files.html')
     self.response.write(template.render(title="User Files", user=self.request.cookies.get("user"), admin=self.request.cookies.get("admin")))
     
-#Play game 1-5
+#Play game 1-6
 class Game(webapp2.RequestHandler):
   def get(self, game_num):
     logging.info("**** Game%s Get ****" % game_num)
@@ -266,7 +271,7 @@ class GiveFeedback(webapp2.RequestHandler):
   def get(self):
     logging.info("**** GiveFeedback Get ****")
     if not (check_admin(self.request.cookies.get("admin")) or check_user(self.request.cookies.get("user"))):
-      self.response.headers.add_header('Set-Cookie','error="You must login as a user in order to continue to this site."; Path=/')
+      self.response.headers.add_header('Set-Cookie','error="You must login as a user in order to give feedback."; Path=/')
       self.response.headers.add_header('Set-Cookie', 'admin=""; Path=/')
       self.response.headers.add_header('Set-Cookie', 'user=""; Path=/')
       self.redirect('/error')
@@ -315,18 +320,6 @@ class Logout (webapp2.RequestHandler):
     self.response.headers.add_header('Set-Cookie', '%s=""; Path=/' % logout_from)
     self.redirect('/%s' % logout_from)
   
-#Show all feed back to admin
-class ViewFeedback(webapp2.RequestHandler):
-  def get(self):
-    logging.info("**** ViewFeedback Get ****")
-    if not check_admin(self.request.cookies.get('admin')):
-      self.response.headers.add_header('Set-Cookie', 'error="You are not authorized to view this page. Please login as an admin."; Path=/')
-      self.response.headers.add_header('Set-Cookie', 'admin=""; Expires=%s' % (datetime.datetime.now() + datetime.timedelta(-1)))
-      self.redirect('/error')
-    all_feedback = db.GqlQuery("SELECT * FROM Feedback ORDER BY created")
-    template = JINJA_ENVIRONMENT.get_template('templates/show_feedback.html')
-    self.response.write(template.render(title="View Feedback", user=self.request.cookies.get("user"), admin=self.request.cookies.get("admin"), all_feedback=all_feedback))
-
 #Login as a user
 class UserLogin(webapp2.RequestHandler):
   def get(self):
@@ -358,6 +351,18 @@ class UserLogin(webapp2.RequestHandler):
     template = JINJA_ENVIRONMENT.get_template('templates/form.html')
     self.response.write(template.render(title="Login",user=self.request.cookies.get("user"), admin=self.request.cookies.get("admin"), **template_values))   
   
+#Show all feed back to admin
+class ViewFeedback(webapp2.RequestHandler):
+  def get(self):
+    logging.info("**** ViewFeedback Get ****")
+    if not check_admin(self.request.cookies.get('admin')):
+      self.response.headers.add_header('Set-Cookie', 'error="You are not authorized to view this page. Please login as an admin."; Path=/')
+      self.response.headers.add_header('Set-Cookie', 'admin=""; Expires=%s' % (datetime.datetime.now() + datetime.timedelta(-1)))
+      self.redirect('/error')
+    all_feedback = db.GqlQuery("SELECT * FROM Feedback ORDER BY created")
+    template = JINJA_ENVIRONMENT.get_template('templates/show_feedback.html')
+    self.response.write(template.render(title="View Feedback", user=self.request.cookies.get("user"), admin=self.request.cookies.get("admin"), all_feedback=all_feedback))
+
 application = webapp2.WSGIApplication([
   ('/', Default),
   ('/about', About),
